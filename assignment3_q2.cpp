16dc5ff79ee784d855be28ac399bb51cec80d6bc
Codingis2much4me <120707072+Codingis2much4me@users.noreply.github.com>
2023-08-23 01:23:59 +0530

    Update assignment3_q2.cpp
    Comments for q2

--- assignment3_q2.cpp
+++ assignment3_q2.cpp
@@ -13,7 +13,7 @@ void swap(char arr[], int i, int j){
 }
 
 void bubble_sort(char arr[], int size){
-    int swapped = 0;
+    int swapped = 0;    //Variable to check if array is already sorted or not.
     for (int i=0; i<size; i++){
         for (int j=0; j<size-i-1; j++){
             if (arr[j] > arr[j+1]){
@@ -22,7 +22,7 @@ void bubble_sort(char arr[], int size){
             }
         }
         printarray(arr, size);
-        if (!swapped) return;
+        if (!swapped) return;   //if array is sorted already then simply terminate the function.
     }
 }
 
@@ -40,17 +40,17 @@ void insertion_sort(char arr[], int size){
 }
 
 int Union(char arr1[], char arr2[], char arr3[], int size_arr1, int size_arr2){
-    int index = 0;
-    int count_arr[256];
+    int index = 0;  //storing the indices of the new array.
+    int count_arr[256];     //All possible ASCII Values.
     for (int i=0; i<256; i++){
-        count_arr[i] = 0;
+        count_arr[i] = 0; //All are initialized to zero first.
     }
 
     for (int x=0; x<size_arr1; x++){
-        if (count_arr[arr1[x]] == 0){
-            arr3[index] = arr1[x];
+        if (count_arr[arr1[x]] == 0){  //Checking whether the element is not already present.
+            arr3[index] = arr1[x];  //Updating the array to store the union with the element.
             index++;
-            count_arr[arr1[x]]++;
+            count_arr[arr1[x]]++; //incrementing
         }
     }
     for (int y=0; y<size_arr2; y++){
@@ -68,7 +68,7 @@ int Union(char arr1[], char arr2[], char arr3[], int size_arr1, int size_arr2){
 
 int main() {
     int n1;
-    printf("n1= ");
+    printf("n1= ");  //Variable to store the size of array1
     scanf("%d", &n1);
     char arr1[n1];
     printf("Enter character arr1: ");
@@ -77,7 +77,7 @@ int main() {
     }
     
     int n2;
-    printf("n2= ");
+    printf("n2= ");  //Size of array 2
     scanf("%d", &n2);
     char arr2[n2];
     printf("Enter character arr2: ");
@@ -92,6 +92,6 @@ int main() {
     printf("Union of arr1 and arr2: \n");
     char(arr3[2*MAX_SIZE]);
     
-    printf("The size of arr3 is %d.", Union(arr1, arr2, arr3, n1, n2));
+    printf("The size of arr3 is %d.", Union(arr1, arr2, arr3, n1, n2)); //Returning the size of the union of the two arrays.
     return 0;
 }
